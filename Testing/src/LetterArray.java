d0851ee7cacb0cfa668aff4f3d32d684dae7ca5c
Thach-s3927021 <93109265+NgoTranBaoThach@users.noreply.github.com>
2023-04-18 23:00:39 +0700

    Add files via upload

--- Testing/src/LetterArray.java
+++ Testing/src/LetterArray.java
@@ -8,28 +8,21 @@ public class LetterArray {
         this.size = size;
     }
 
-    public void print(){
+    private void print(){
         for (Letter letter : array) {
             System.out.print(letter.getLetter() + ":" + letter.getFrequency() + ", ");
         }
         System.out.println();
     }
 
-    public void add(char c, int freq, int index){
+    protected void add(char c, int freq, int index){
         Letter letter = new Letter(c,freq);
         this.array[index] = letter;
     }
 
     public void sortDescending(){
-        for(int i = 0; i < size - 1; i++){
-            for(int j = i + 1; j < size; j++){
-                if(array[i].getFrequency() < array[j].getFrequency()){
-                    Letter tmp = array[i];
-                    array[i] = array[j];
-                    array[j] = tmp;
-                }
-            }
-        }
+        mergeSort(array, 0, array.length);
+
         for(int i = 0; i < array.length; i++){
             if(array[i].getFrequency() == 0) {
                 lastAvailableElement = i-1;
@@ -37,6 +30,34 @@ public class LetterArray {
             }
         }
         lastAvailableElement = array.length - 1;
+    }
+
+    public void mergeSort(Letter[] input, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+        int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
+
+    public void merge(Letter[] input, int start, int mid, int end) {
+
+        if (input[mid - 1].getFrequency() > input[mid].getFrequency()) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+        Letter[] temp = new Letter[end - start];
+        while (i < mid && j < end) {
+            temp[tempIndex++] = input[i].getFrequency() > input[j].getFrequency() ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
     }
 
     public void deductFrequency(int index){
--- Testing/src/Main.java
+++ Testing/src/Main.java
@@ -1,10 +1,10 @@
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int max = 0;
         for(int i = 1; i < 4; i++){
-            SecretKeyGuess guesser = new SecretKeyGuess();
+            SecretKeyGuesser guesser = new SecretKeyGuesser();
 
             guesser.start();
             if(guesser.counter >= max) max = guesser.counter;
