337ddacecaf0c9154a3ee360593552d17b1379ee
pongnatel <s3877819@rmit.edu.vn>
2023-04-23 17:49:14 +0700

    Clean code and delete Exception

--- Testing/src/Main.java
+++ Testing/src/Main.java
@@ -1,22 +1,17 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         int max = 0;
-        for(int i = 1; i < 10000; i++){
+        for(int i = 1; i < 100000; i++){
             System.out.println("Attempt " + i);
             SecretKeyGuesser guesser = new SecretKeyGuesser();
 
             guesser.start();
-            if(guesser.counter >= max) max = guesser.counter;
-//            max += guesser.counter;
+//            if(guesser.counter >= max) max = guesser.counter;
+            max += guesser.counter;
         }
 
-//        double avg = ( (double) max/ (double) 100000);
-//        System.out.println("Max guess: " + avg);
-        System.out.println("Max guess: " + max);
-
-
-//        SecretKeyGuess guesser = new SecretKeyGuess();
-//
-//        guesser.start();
+        double avg = ( (double) max/ (double) 100000);
+        System.out.println("Max guess: " + avg);
+//        System.out.println("Max guess: " + max);
     }
 }
