6521213f4f6d10cde3e5174bafb984b8ad998707
pongnatel <s3877819@rmit.edu.vn>
2023-05-01 17:11:43 +0700

    Add max guess measure to class Main

--- Testing/src/Main.java
+++ Testing/src/Main.java
@@ -1,19 +1,22 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         int max = 0;
+        int sum = 0;
 
         // Try 100000 different passwords
-        for(int i = 1; i < 100000; i++){
+        for(int i = 1; i <= 1000000; i++){
             System.out.println("Attempt " + i);
             SecretKeyGuesser guesser = new SecretKeyGuesser();
             // Activate the start() method
             guesser.start();
             // Count the total number of guesses
-            max += guesser.counter;
+            sum += guesser.counter;
+            if(max < guesser.counter) max = guesser.counter;
         }
 
         // Calculate the average number of guesses
-        double avg = ( (double) max/ (double) 100000);
+        double avg = ( (double) sum/ (double) 1000000);
         System.out.println("Average guess: " + avg);
+        System.out.println("Max guess: " + max);
     }
 }
